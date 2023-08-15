105a8861707b0f1b1b84825daf957db74eaa8d56
testGit <test@Git.com>
2023-08-15 16:23:18 +0530

    Sum of all sub arrays in an Array

--- sumSubArrSum.js
+++ sumSubArrSum.js
@@ -11,8 +11,10 @@ function sumSubArrSum(A){
     let sum = 0;
 
     for(let i=0; i<n; i++){
-        let occurences = (i+1) * (n-i);
+        let occurences = (i+1) * (n-i); // (start index) * (end index)
         sum = sum + A[i] * occurences;
+
+        //Occurences are calculated because when we parse all the subarrays, it just means combining all the elements as per their occurences. Here, occurences = (number of elements till the current element in the loop) * (elements after the current element in the loop). Example: the occurence of 6 in all sub arrays would be counted with always s = [0,4] & [4,7] => 5 * 4
     }
 
     return sum;
