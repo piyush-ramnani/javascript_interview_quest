2556ffca2beea53625d7c695917f33b5a9a1ab2d
testGit <test@Git.com>
2023-11-03 13:31:58 +0530

    Inheritance | OOP | Interview questions

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # javascript_interview_quest
+
 Javascript interview questions 🤖
+
+This is where I have marked all the important qustions before sitting in an interview for the position of Software Engineer.
+
+The repo contains some DSA some theoretical questions.
+
+The programs are also commented with relevant explainations.
+
+# Topics involved
+
+-Arrays (2D, 3D), bit manipulation, Recursion, OOP etc.
---
+++ inheritance.js
@@ -0,0 +1,46 @@
+/*
+To create a class inheritance, use the extends keyword.
+
+A class created with a class inheritance inherits all the methods from another class
+
+Note: setTimeOut should have a function as an argument not methods directly!
+*/
+
+class Cars {
+  constructor(brand) {
+    this.carName = brand;
+  }
+  start() {
+    console.log(`${this.carName} engine grouls`);
+  }
+  drive() {
+    console.log("It zooms away into the thin air");
+  }
+}
+
+//By calling the super() method in the constructor method, we call the parent's constructor method and gets access to the parent's properties and methods.
+class Race extends Cars {
+  constructor(brand) {
+    super(brand);
+  }
+
+  siren() {
+    console.log("Police has arrived!");
+  }
+
+  fly() {
+    this.start();
+    this.drive();
+  }
+}
+
+let myCar = new Race("Mustang");
+let richKid = new Race("Supra");
+
+console.log(myCar.siren());
+setTimeout(() => myCar.fly(), 2000);
+setTimeout(() => richKid.fly(), 3000);
+
+/*
+myCar.fly() & richKid.fly() are executed immediately, without any delay, and it logs the results right away. The setTimeout function receives the result of calling the function as its first argument, and in this case, which is why result is undefined. So, effectively, you are using setTimeout(undefined, 2000)
+*/
